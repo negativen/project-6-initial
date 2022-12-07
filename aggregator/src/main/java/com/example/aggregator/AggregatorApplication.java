93534a558f8ccda98ce4f1c9679e31e9090e2b04
negativen <wilsnat94@gmail.com>
2022-12-06 19:33:55 -0700

    project 6

--- aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
@@ -2,12 +2,23 @@ package com.example.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class AggregatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AggregatorApplication.class, args);
+        ConfigurableApplicationContext cxt = SpringApplication.run(AggregatorApplication.class, args);
+        String[] beans = cxt.getBeanDefinitionNames();
+        AtomicInteger counter = new AtomicInteger(1);
+        Arrays.stream(beans)
+                .sorted()
+                .map(name -> counter.getAndIncrement() + ": " + name)
+                .forEach(System.out::println);
+
     }
 
 }
--- dictionary/src/main/java/com/example/dictionary/reference/DictionaryReference.java
+++ dictionary/src/main/java/com/example/dictionary/reference/DictionaryReference.java
@@ -7,27 +7,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class DictionaryReference {
 
-    private static final Logger logger = LoggerFactory.getLogger(DictionaryReference.class).getName();
+    private static final Logger logger = LoggerFactory.getLogger(DictionaryReference.class.getName());
 
     private static Map<String, String> dictionary;
 
     static {
-
         try {
             readDictionaryFile();
         } catch (JsonProcessingException e) {
-            logger.error("Failed to read dictionary file");
+            logger.error("There was a problem reading the dictionary file");
         }
     }
 
     private DictionaryReference() {
-        //stops instantiation
+
     }
 
     private static void readDictionaryFile() throws JsonProcessingException {
@@ -37,11 +39,10 @@ public class DictionaryReference {
 
         InputStream inputStream = DictionaryReference.class.getClassLoader()
                 .getResourceAsStream("dictionary.json");
-
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String json = bufferedReader.lines()
-                .collect(Colllectors.joining("\n"));
+                .collect(Collectors.joining("\n"));
 
         ObjectMapper mapper = new ObjectMapper();
         dictionary = mapper.readValue(json, Map.class);
@@ -49,16 +50,18 @@ public class DictionaryReference {
         sw.stop();
         long milliseconds = sw.getLastTaskTimeMillis();
 
-        String message = new StringBuilder().append("Dictionary created with")
-                .append(dictionary.size)
+        String message = new StringBuilder().append("Dictionary created with ")
+                .append(dictionary.size())
                 .append(" entries in ")
                 .append(milliseconds)
                 .append("ms")
                 .toString();
-
         logger.info(message);
+
     }
+
     public static Map<String, String> getDictionary() {
         return DictionaryReference.dictionary;
     }
+
 }
